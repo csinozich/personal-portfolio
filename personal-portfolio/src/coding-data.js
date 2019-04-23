de423bab0d91e0ddbfa616e0108b2445e0ac0cfb
Claire Sinozich <claire.sinozich@gmail.com>
2019-04-23 09:46:49 -0600

    add project

--- personal-portfolio/src/coding-data.js
+++ personal-portfolio/src/coding-data.js
@@ -1,12 +1,21 @@
 export default [
   {
+    name: 'Personal Portfolio',
+    github: 'https://github.com/csinozich/personal-portfolio',
+    netlify: 'claire-s.codes',
+    description: 'This portfolio is one of my own original projects and designs! You can check out the code in my GitHub.',
+    imageUrl: 'https://live.staticflickr.com/65535/40711992463_ca64efc93c.jpg',
+    languages: 'HTML, CSS, React, React Router',
+    id: 0
+  },
+  {
     name: 'Productivity Application',
     github: 'https://github.com/csinozich/React-Todo',
     netlify: 'https://cranky-goldberg-4fda75.netlify.com/',
     description: 'This Productivity Application was built out of a project for Lambda School. It is a basic application with a to-do list and Pomodoro timer, and a little basic styling. You can see a tutorial for this project in the writing tab below. This was made in collaboration with Douglas Jordan (www.douglasjordan.me).',
     imageUrl: "https://live.staticflickr.com/65535/40704529703_7d91084390.jpg",
     languages: 'HTML, CSS, React, React Router',
-    id: 0,
+    id: 1,
   },
   {
     name: 'S&J Architects',
@@ -15,6 +24,6 @@ export default [
     description: 'This website was made as a test for HTML, CSS, and JavaScript skills at Lambda School. It is a basic portfolio-type website. This project was not designed by me.',
     imageUrl: 'https://live.staticflickr.com/65535/47618123442_9ecd31bba0.jpg',
     languages: 'CSS, HTML, JavaScript, LESS',
-    id: 1,
+    id: 2,
   }
 ]
