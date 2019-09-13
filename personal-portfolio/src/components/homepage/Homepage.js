1c6cb742c580f3ac2ef19aadc86ff4b6517aa815
Claire Sinozich <claire.sinozich@gmail.com>
2019-09-13 08:32:01 -0600

    homepage changes

--- personal-portfolio/src/components/homepage/Homepage.js
+++ personal-portfolio/src/components/homepage/Homepage.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './homepage.css'
+import React from "react";
+import "./homepage.css";
 
 const Homepage = props => {
   return (
     <div>
-      <h2 className='homepage-header'>Hi, my name is Claire.</h2>
-      <h3 className='homepage-text'>I'm a farmer, activist, and aspiring web developer. I want to make the world a better place, one line of code at a time.</h3>
+      <h2 className="homepage-header">Hi, my name is Claire.</h2>
+      <h3 className="homepage-text">
+        I'm a product manager, aspiring web developer, farmer, and activist. I
+        want to make the world a better place, one line of code at a time.
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
