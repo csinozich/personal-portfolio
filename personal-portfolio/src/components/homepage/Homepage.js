c6f26d1c698b314d756572aaa4d710037a586527
Claire Sinozich <claire.sinozich@gmail.com>
2020-01-27 10:27:37 -0500

    small design tweaks

--- personal-portfolio/src/components/homepage/Homepage.js
+++ personal-portfolio/src/components/homepage/Homepage.js
@@ -6,8 +6,9 @@ const Homepage = props => {
     <div>
       <h2 className="homepage-header">Hi, my name is Claire.</h2>
       <h3 className="homepage-text">
-        I'm a product manager, aspiring web developer, farmer, and activist. I
-        want to make the world a better place, one line of code at a time.
+        I'm a junior product manager, content creator, software developer,
+        designer, farmer, and activist. I want to make the world a better place,
+        one line of code at a time.
       </h3>
     </div>
   );
